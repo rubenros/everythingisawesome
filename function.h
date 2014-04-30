35500b949e800303f85e63d9ef7243afc86fb167
unknown <ruben.ros@CS300-3D.acad.fullerton.edu>
2014-04-30 11:35:55 -0700

    function.h

---
+++ function.h
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class everythingisawesome
+{
+public:
+	everythingisawesome();
+	~everythingisawesome();
+	void hello();
+
+
+};
+
+everythingisawesome::everythingisawesome()
+{
+}
+
+everythingisawesome::~everythingisawesome()
+{
+
+}
+
+void everythingisawesome::hello()
+{
+	std::cout << "hello" << std::endl;
+}
